c5dd9f555f7cf28b2b89ea97191054c965ff8dca
Jose <jose.cortesvarela@mail.utoronto.ca>
2016-05-04 11:22:41 -0400

    Add ctakes as a service for annotations.

---
+++ ctakes-service/src/main/java/org/phenotips/textanalysis/internal/CTakesLoader.java
@@ -0,0 +1,159 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+package org.phenotips.textanalysis.internal;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.util.Version;
+
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLObject;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.util.OWLOntologyWalker;
+
+
+/**
+ * Loads the HPO into a lucene index for use by ctakes.
+ *
+ * @version $Id$
+ */
+public class CTakesLoader
+{
+    /**
+     * A visitor to go through ontology elements.
+     */
+    private CTakesOWLVisitor visitor;
+
+    /**
+     * A walker for the visitor.
+     */
+    private OWLOntologyWalker walker;
+
+    /**
+     * Our ontology manager.
+     */
+    private OWLOntologyManager manager;
+
+    /**
+     * Where on the filesystem the ontology is.
+     */
+    private String location;
+
+    /**
+     * The owl ontology object.
+     */
+    private OWLOntology ontology;
+
+    /**
+     * The lucene index writer.
+     */
+    private IndexWriter indexWriter;
+
+    /**
+     * The lucene indexDirectory.
+     */
+    private Directory indexDirectory;
+
+    /**
+     * Construct a new loader.
+     *
+     * @param ontologyLocation where we can find the ontology.
+     * @param indexDirectory the directory where we can find the index
+     * @throws IOException if lucene throws on index writer creation
+     */
+    public CTakesLoader(String ontologyLocation, Directory indexDirectory) throws IOException
+    {
+        manager = OWLManager.createOWLOntologyManager();
+        location = ontologyLocation;
+        ontology = getOntology();
+        Set<OWLOntology> set = new HashSet<>(1);
+        set.add(ontology);
+        walker = new OWLOntologyWalker(set);
+        visitor = new CTakesOWLVisitor(walker);
+        /* Ontology doesn't look like a word anymore... */
+        this.indexDirectory = indexDirectory;
+        indexWriter = createIndexWriter();
+    }
+
+    /**
+     * Create a new index writer.
+     * @return the index writer
+     */
+    private IndexWriter createIndexWriter() throws IOException
+    {
+        Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_40);
+        IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_40, analyzer);
+        return new IndexWriter(indexDirectory, config);
+    }
+
+    /**
+     * Load the HPO into a lucene index.
+     */
+    public void load()
+    {
+        try {
+            ontology.accept(visitor);
+            for (OWLObject object : ontology.getNestedClassExpressions()) {
+                object.accept(visitor);
+            }
+            for (OWLObject object : ontology.getClassesInSignature()) {
+                object.accept(visitor);
+            }
+            for (OWLObject object : ontology.getAxioms()) {
+                object.accept(visitor);
+            }
+            indexWriter.addDocuments(visitor.getDocuments());
+        } catch (IOException e) {
+            /* XXX */
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Close this loader.
+     * @throws IOException if lucene throws.
+     */
+    public void close() throws IOException
+    {
+        indexWriter.commit();
+        indexWriter.close();
+    }
+
+    /**
+     * Construct an owl ontology and return it.
+     */
+    private OWLOntology getOntology()
+    {
+        try {
+            return manager.loadOntologyFromOntologyDocument(new File(location));
+        } catch (OWLOntologyCreationException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+}
---
+++ ctakes-service/src/main/java/org/phenotips/textanalysis/internal/HPOConsumer.java
@@ -0,0 +1,113 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+package org.phenotips.textanalysis.internal;
+
+import java.util.Iterator;
+import java.util.Properties;
+
+import org.apache.ctakes.dictionary.lookup.MetaDataHit;
+import org.apache.ctakes.dictionary.lookup.ae.BaseLookupConsumerImpl;
+import org.apache.ctakes.dictionary.lookup.vo.LookupHit;
+import org.apache.ctakes.typesystem.type.constants.CONST;
+import org.apache.ctakes.typesystem.type.refsem.Entity;
+import org.apache.ctakes.typesystem.type.refsem.OntologyConcept;
+import org.apache.ctakes.typesystem.type.textsem.EntityMention;
+import org.apache.uima.UimaContext;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.cas.FSArray;
+
+/**
+ * A consumer that takes dictionary hits and adds them to a jcas as ontology concepts mapped to the hpo.
+ *
+ * @version $Id$
+ */
+public class HPOConsumer extends BaseLookupConsumerImpl
+{
+    /**
+     * The name of the field containing the id name in the properties for this instance.
+     */
+    private static final String ID_PRP_KEY = "idname";
+
+    /**
+     * The name of the field containing the coding scheme in the properties for this instance.
+     */
+    private static final String CODING_SCHEME_PRP_KEY = "codingScheme";
+
+    private static final String LABEL_PRP_KEY = "labelname";
+
+    /**
+     * The properties for this instance.
+     */
+    private Properties properties;
+
+    /**
+     * The maximum number of elements.
+     */
+    private int maxListSize;
+
+    /**
+     * CTOR.
+     * @param aCtx the UimaContext
+     * @param properties this object's properties
+     * @param maxListSize the maximum number of elements.
+     */
+    public HPOConsumer(UimaContext aCtx, Properties properties, int maxListSize)
+    {
+        this.properties = properties;
+        this.maxListSize = maxListSize;
+    }
+
+    @Override
+    public void consumeHits(JCas jcas, Iterator<LookupHit> hits)
+    {
+        while (hits.hasNext()) {
+            LookupHit hit = hits.next();
+            OntologyConcept concept = getOntologyConcept(jcas, hit);
+            Entity e = new Entity(jcas);
+            EntityMention annotation = new EntityMention(jcas, hit.getStartOffset(), hit.getEndOffset());
+            FSArray array = new FSArray(jcas, 1);
+            array.set(0, concept);
+
+            e.setOntologyConcept(concept);
+            annotation.setDiscoveryTechnique(CONST.NE_DISCOVERY_TECH_DICT_LOOKUP);
+            annotation.setOntologyConceptArr(array);
+            annotation.setEntity(e);
+            annotation.addToIndexes();
+        }
+    }
+
+    /**
+     * Create an ontology concept array for the lookup hit given.
+     * @param jcas the jcas instance
+     * @param hit the lookup hit to create the concept array for
+     */
+    private OntologyConcept getOntologyConcept(JCas jcas, LookupHit hit)
+    {
+        String codingScheme = properties.getProperty(CODING_SCHEME_PRP_KEY);
+        String idname = properties.getProperty(ID_PRP_KEY);
+        String labelName = properties.getProperty(LABEL_PRP_KEY);
+        MetaDataHit mdh = hit.getDictMetaDataHit();
+        String id = mdh.getMetaFieldValue(idname);
+        OntologyConcept concept = new OntologyConcept(jcas);
+        concept.setCode(id);
+        /* I'm not sure what the oui is supposed to be, but it's not being used elsewhere in the pipeline */
+        concept.setOui(mdh.getMetaFieldValue(labelName));
+        concept.setCodingScheme(codingScheme);
+        return concept;
+    }
+}
